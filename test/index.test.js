4d3f3aef2d64b4eb083e80a79b0b65c94d6dc050
Mehmet Seven <mehmetseven0@gmail.com>
2021-10-21 13:52:29 +0300

    mocha & chai unit testing

---
+++ test/index.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+
+const server = require('../app');
+
+describe('Node.JS Server Test', () => {
+  it('Anasayfa 200 http koduyla response dönmelidir.', (done) => {
+    chai
+      .request(server)
+      .get('/')
+      .end((error, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
